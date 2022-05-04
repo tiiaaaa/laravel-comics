a8265a4f59bfd799fe180f1ef4ee943557d6385b
MattiaPortanti <mattia.portantipm@gmail.com>
2022-05-04 17:11:55 +0200

    creato all'interno della cartella partials diversi component (heder, main, footer) per atomizzare il component princiaple ovvero homepage.blade. problemi con traduzione di app.scss in app.css

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Comics</title>
-    <link rel="stylesheet" href="{{assets('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-
+    {{-- Header --}}
     @include('partials.header')
 
+    {{-- Main --}}
+    @include('partials.main')
 
+    {{-- Footer --}}
     @include('partials.footer')
 
 </body>
